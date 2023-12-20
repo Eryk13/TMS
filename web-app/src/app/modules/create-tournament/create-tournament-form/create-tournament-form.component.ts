62052f09461394166099ed754b1b9e67b6c70372
Eryk <eryk9903@gmail.com>
2023-12-20 14:52:17 +0100

    style tournament form

--- web-app/src/app/modules/create-tournament/create-tournament-form/create-tournament-form.component.ts
+++ web-app/src/app/modules/create-tournament/create-tournament-form/create-tournament-form.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-tournament-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './create-tournament-form.component.html',
   styleUrl: './create-tournament-form.component.css'
 })
-export class CreateTournamentFormComponent {
+export class CreateTournamentFormComponent implements OnInit {
+
+  form = this.fb.group({
+    name: [''],
+    type: ['Type'],
+    discipline: ['Discipline', ],
+    participantType: [''],
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
 
 }
